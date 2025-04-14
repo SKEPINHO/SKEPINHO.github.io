038fe3827f7820bceae36030857ac300864b74c3
SKEPINHO <pepazvejk@gmail.com>
2025-04-14 20:36:34 +0200

    pdat

--- slider.js
+++ slider.js
@@ -4,12 +4,7 @@ const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
 let index = 0;
 
-function showSlide(i) {
-  slides.forEach((slide, idx) => {
-    slide.classList.toggle('active', idx === i);
-    dots[idx].classList.toggle('active', idx === i);
-  });
-}
+
 
 function nextSlide() {
   index = (index + 1) % slides.length;
